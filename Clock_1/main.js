f68be055c8969afabdb3d6e3bce2af82c42e43cd
Alex777 <shevashokc@gmail.com>
2018-03-18 13:22:40 +0200

    added numbers, clock hands(second, minute, hour)

--- Clock_1/main.js
+++ Clock_1/main.js
@@ -40,13 +40,54 @@ function displayCanvas() {
 // цыфры для часов
 for( var th = 1; th <=12; th ++ ){
   ctx.beginPath();
-  ctx.font = 'bold 25 sans-serif';
+  ctx.font = 'bold 25px sans-serif';
   var xText = xCenterClock + (radiusNum - 30) * Math.cos(-30 * th * (Math.PI/180) + Math.PI/2 );
-  var yText = xCenterClock + (radiusNum - 30) * Math.sin(-30 * th * (Math.PI/180) + Math.PI/2);
-  ctx.strokeText(th, xText, yText);
+  var yText = yCenterClock - (radiusNum - 30) * Math.sin(-30 * th * (Math.PI/180) + Math.PI/2);
+  if( th <=9 ){
+    ctx.strokeText(th, xText - 5, yText + 10);
+  }else {
+    ctx.strokeText(th, xText - 15, yText + 10 );
+  };
   ctx.stroke();
   ctx.closePath();
 };
+
+// стрелки часов
+var lengthSecond = radiusNum - 10;
+var lengthMinutes = radiusNum - 25;
+var lengthHours = lengthMinutes / 1.3;
+var d = new Date();
+var t_sec = 6 * d.getSeconds();
+var t_min = 6 * ( d.getMinutes() + (1/60) * d.getSeconds() );
+var t_hor = 30 * ( d.getHours() + (1/60) * d.getMinutes() )
+// рисуем струлку секунд
+ctx.beginPath();
+ctx.strokeStyle = '#FF0000';
+ctx.lineWidth = 3;
+ctx.moveTo(xCenterClock, yCenterClock);
+ctx.lineTo(xCenterClock + lengthSecond * Math.cos( Math.PI/2 - t_sec * ( Math.PI/180 ) ),
+          yCenterClock - lengthSecond * Math.sin( Math.PI/2 - t_sec * ( Math.PI/180 ) ));
+ctx.stroke();
+ctx.closePath();
+// рисуем стрелку минут
+ctx.beginPath();
+ctx.strokeStyle = '#000000';
+ctx.lineWidth = 5;
+ctx.moveTo(xCenterClock, yCenterClock);
+ctx.lineTo(xCenterClock + lengthMinutes * Math.cos( Math.PI/2 - t_min * ( Math.PI/180 ) ),
+          yCenterClock - lengthMinutes * Math.sin( Math.PI/2 - t_min * ( Math.PI/180 ) ));
+ctx.stroke();
+ctx.closePath();
+// рисуем стрклку часов
+ctx.beginPath();
+ctx.strokeStyle = '#000000';
+ctx.lineWidth = 7;
+ctx.moveTo(xCenterClock, yCenterClock);
+ctx.lineTo(xCenterClock + lengthHours * Math.cos( Math.PI/2 - t_hor * ( Math.PI/180 ) ),
+          yCenterClock - lengthHours * Math.sin( Math.PI/2 - t_hor * ( Math.PI/180 ) ));
+ctx.stroke();
+ctx.closePath();
+return;
 };
 
 
